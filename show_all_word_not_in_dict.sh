1b9a2e7f2ab21eea48d31f834bfb7817d4962139
Andrey Balabokhin <balabokhin@correlatedsolutions.com>
2022-04-16 07:08:32 -0400

    Make seqrch for TRA as well in show_all_word_not_in_dict.sh script

--- show_all_word_not_in_dict.sh
+++ show_all_word_not_in_dict.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-a=`find -name "*.tra" -exec hunspell -d ru_RU -l {} \; | sort -u`
+a=`find -iname "*.tra" -exec hunspell -d ru_RU -l {} \; | sort -u`
 for s in $a
 do
-	b=`grep "\b$s\b" *.tra | tr -d '\n'`
+	b=`grep "\b$s\b" *.tra *.TRA | tr -d '\n'`
 	echo "$s $b"
 done
