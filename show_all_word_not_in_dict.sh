7d2ab8703def876f22c1bb3e0fedbdc7aedd7321
Andrey Balabokhin <balabokhin@correlatedsolutions.com>
2022-04-21 10:29:11 -0400

    Fix script to find non dictionary words

--- show_all_word_not_in_dict.sh
+++ show_all_word_not_in_dict.sh
@@ -2,6 +2,6 @@
 a=`find -iname "*.tra" -exec hunspell -d ru_RU -l {} \; | sort -u`
 for s in $a
 do
-	b=`grep "\b$s\b" *.tra *.TRA | tr -d '\n'`
+	b=`grep "\b$s\b" *.[t,T][r,R][a,A] | tr -d '\n'`
 	echo "$s $b"
 done
