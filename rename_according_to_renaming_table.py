1f3c0b691119d1a367f1e66799eaef212a271e25
Andrey Balabokhin <abalabokhin@gmail.com>
2023-09-22 14:53:30 -0400

    finish renaming script to add prefixes

--- rename_according_to_renaming_table.py
+++ rename_according_to_renaming_table.py
@@ -7,6 +7,15 @@ import re
 import sys
 
 
+def read_table(filename):
+    table_file = open(filename, "r")
+    table = {}
+    for line in table_file.readlines():
+        tokens = line.split(',')
+        table[tokens[0].strip()] = tokens[1].strip()
+    return table
+
+
 def replace_refs_in_bin(filename, table):
     f = open(filename, "rb")
     s = f.read()
@@ -20,6 +29,7 @@ def replace_refs_in_bin(filename, table):
         s = re.sub(key_b, value_b, s, flags=re.IGNORECASE)
 
     if s != s_orig:
+        print("bin file {} changed and saved".format(filename))
         f = open(filename, "wb")
         f.write(s)
         f.close()
@@ -34,18 +44,27 @@ def replace_refs_in_txt(filename, table):
         s = re.sub('\\b'+key+'\\b', table[key], s, flags=re.IGNORECASE)
 
     if s != s_orig:
+        print("txt file {} changed and saved".format(filename))
         f = open(filename, "w", encoding='cp1251')
         f.write(s)
         f.close()
 
-def change_refs(in_folder, renaming_table_filename, folders_to_skip):
-    table_file = open(renaming_table_filename, "r")
-    table = {}
-    for l in table_file.readlines():
-        tokens = l.split(',')
-        table[tokens[0].strip()] = tokens[1].strip()
 
-    print(table)
+def rename_files_according_to_table(file_table, renaming_table):
+    print(renaming_table)
+    for k in file_table:
+        if k not in renaming_table:
+            continue
+        new_basename = renaming_table[k]
+        path, filename = os.path.split(file_table[k])
+        _, ext = os.path.splitext(filename)
+        new_full_filename = os.path.join(path, new_basename + ext)
+        print("renaming {} into {}".format(file_table[k], new_full_filename))
+        os.rename(file_table[k], new_full_filename)
+
+
+def change_refs(in_folder, renaming_table, folders_to_skip):
+    print(renaming_table)
     # check, do we need eff?
     es_bin = ['are', 'cre', 'eff', 'itm', 'pro', 'spl', 'sto', 'vvs']
     ex_txt = ['d', 'tph', 'baf', 'tra', 'tp2']
@@ -59,80 +78,9 @@ def change_refs(in_folder, renaming_table_filename, folders_to_skip):
             for file in files:
                 split_filename = os.path.splitext(file)
                 if split_filename[1].upper() in extensions_bin:
-                    print("processing binary file {}".format(file))
-                    replace_refs_in_bin(os.path.join(dir_, file), table)
+                    replace_refs_in_bin(os.path.join(dir_, file), renaming_table)
                 if split_filename[1].upper() in extensions_txt:
-                    print("processing txt file {}".format(file))
-                    replace_refs_in_txt(os.path.join(dir_, file), table)
-
-def clean_id(id_to_clean):
-    result = id_to_clean
-    for c in ["_", "-", "#"]:
-        i = result.find(c)
-        if 0 <= i <= 2:
-            result = result[i + 1:]
-        result = result.replace(c, '')
-    return result
-
-
-def build_table(ids, prefix):
-    table = {}
-    for id in ids:
-        id_cleaned = clean_id(id)
-        if id.startswith(prefix):
-            continue
-        i = id.find(prefix)
-        if i >= 0:
-            value = prefix + id[:i] + id[i+len(prefix):]
-        elif len(id) <= 8 - len(prefix):
-            value = prefix + id
-        elif len(id_cleaned) <= 8 - len(prefix):
-            value = prefix + id_cleaned
-        else:
-            n_to_eat = len(prefix) + len(id_cleaned) - 8
-            n_keep_at_end = 1
-            result = re.search("[0-9]+$", id_cleaned)
-            if result:
-                n_keep_at_end = len(result.group())
-            else:
-                result = re.search("[0-9]+.$", id_cleaned)
-                if result:
-                    n_keep_at_end = len(result.group())
-            if n_to_eat + n_keep_at_end > 8:
-                n_keep_at_end = n_to_eat + n_keep_at_end - 8
-            value = prefix + id_cleaned[0: len(id_cleaned) - n_to_eat - n_keep_at_end]
-            if n_keep_at_end > 0:
-                value += id_cleaned[-n_keep_at_end:]
-        table[id] = value
-
-    values = set(table.values())
-    v2k = {}
-    for k in table:
-        v = table[k]
-        if v in v2k:
-            # try to solve the collisions
-            new_v = v
-            for j in range(100):
-                suffix = str(j)
-                if len(suffix) + len(v) <= 8:
-                    v_candidate = v + suffix
-                else:
-                    n_to_eat = len(v) + len(suffix) - 8
-                    v_candidate = v[0:-n_to_eat] + suffix
-                if v_candidate not in values:
-                    new_v = v_candidate
-                    break
-
-            if new_v == v:
-                print("UNSOLVED COLLISION {}->{}, {}->{}".format(v2k[v], v, k, v))
-            v = new_v
-        if len(v) > 8:
-            print("Error in defining new id name: ", k, v)
-        values.add(v)
-        v2k[v] = k
-        table[k] = v
-
-    return table
+                    replace_refs_in_txt(os.path.join(dir_, file), renaming_table)
 
 
 __desc__ = '''
@@ -149,6 +97,7 @@ if __name__ == '__main__':
     parser.add_argument('--skip-folders', help='Case sensitive folder names to skip', nargs='+', default=[])
     args = parser.parse_args()
 
-    ids_with_filenames = change_refs(args.in_folder, args.in_table_file, args.skip_folders)
-    # print(ids_with_filenames)
-
+    file_table = read_table(args.in_filenames_file)
+    renaming_table = read_table(args.in_table_file)
+    change_refs(args.in_folder, renaming_table, args.skip_folders)
+    rename_files_according_to_table(file_table, renaming_table)
