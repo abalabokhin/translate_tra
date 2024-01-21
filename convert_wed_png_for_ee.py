d4f5c71cbc6f32124e361c7f08b00f34252ddfd5
Andrey Balabokhin <balabokhin@correlatedsolutions.com>
2024-01-20 20:59:34 -0500

    add more test data

--- convert_wed_png_for_ee.py
+++ convert_wed_png_for_ee.py
@@ -7,11 +7,38 @@ import sys
 import chardet
 import re
 
+
+def find_groups(elements):
+    def dfs(x, y, group):
+        if (x, y) in visited:
+            return
+        visited.add((x, y))
+        group.append([x, y])
+
+        for dx in [-1, 0, 1]:
+            for dy in [-1, 0, 1]:
+                nx, ny = x + dx, y + dy
+                if (nx, ny) in elements_set and (nx, ny) not in visited:
+                    dfs(nx, ny, group)
+
+    elements_set = set(map(tuple, elements))
+    visited = set()
+    groups = []
+
+    for x, y in elements_set:
+        if (x, y) not in visited:
+            current_group = []
+            dfs(x, y, current_group)
+            groups.append(current_group)
+
+    return groups
+
 __desc__ = '''
 This program takes wed and corresponding png files and convert them both to prepare them to export in EE games on infinity engine.
 '''
 
 if __name__ == '__main__':
+    # still don't know what to do with fireplaces
     parser = argparse.ArgumentParser(description=__desc__)
     parser.add_argument('infile1', help='WED file')
     parser.add_argument('out_dir', help='dir to put transformed wed and png files', default="")
@@ -19,7 +46,9 @@ if __name__ == '__main__':
 
     wed_in = open(args.infile1, mode="rb")
     wed_data = wed_in.read()
-    png_filename = os.path.splitext(args.infile1)[0] + ".PNG"
+    filepath_no_ext = os.path.splitext(args.infile1)[0]
+    png_filename = filepath_no_ext + ".PNG"
+    basename = os.path.basename(filepath_no_ext).upper()
     png_in = open(png_filename, mode="rb")
 
     n_doors = int.from_bytes(wed_data[0xc:0xc + 4], "little")
@@ -28,17 +57,57 @@ if __name__ == '__main__':
     doors_offset = int.from_bytes(wed_data[0x18:0x18 + 4], "little")
     door_tile_cell_offset = int.from_bytes(wed_data[0x1c:0x1c + 4], "little")
 
+    overlay_found = False
+    for overlay_i in range(n_overlays):
+        overlay_offset = overlays_offset + overlay_i * 0x18
+        overlay_w = int.from_bytes(wed_data[overlay_offset:overlay_offset + 2], "little")
+        overlay_h = int.from_bytes(wed_data[overlay_offset + 0x2:overlay_offset + 0x2 + 2], "little")
+        tileset_name = wed_data[overlay_offset + 0x4:overlay_offset + 0x4 + 8].split(b'\0')[0].decode().upper()
+        tilemap_offset = int.from_bytes(wed_data[overlay_offset + 0x10:overlay_offset + 0x10 + 4], "little")
+        tile_index_lookup_offset = int.from_bytes(wed_data[overlay_offset + 0x14:overlay_offset + 0x14 + 4], "little")
+        if tileset_name == basename:
+            overlay_found = True
+            break
+
+    if not overlay_found:
+        sys.exit("Cannot find appropriate overlay name in WED file")
+
+    tile_map = {}
+    for tile_i in range(overlay_w * overlay_h):
+        tilemap_offset_i = tilemap_offset + tile_i * 0xa
+        tile_start_i = int.from_bytes(wed_data[tilemap_offset_i:tilemap_offset_i + 2], "little")
+        tile_count = int.from_bytes(wed_data[tilemap_offset_i + 0x2:tilemap_offset_i + 0x2 + 2], "little")
+        second_tile_offset = tilemap_offset_i + 0x4
+        second_tile = int.from_bytes(wed_data[second_tile_offset:second_tile_offset + 2], "little", signed=True)
+        if second_tile < 0:
+            continue
+
+        for tile_ii in range(tile_count):
+            tile_ii_offset = tile_index_lookup_offset + (tile_start_i + tile_ii) * 2
+            first_tile = int.from_bytes(wed_data[tile_ii_offset:tile_ii_offset + 2], "little")
+            tile_map[first_tile] = [second_tile, second_tile_offset]
+
+    elements = []
+    for k in tile_map.keys():
+        elements.append([k % overlay_w, k // overlay_w])
+    print(elements)
+    print(find_groups(elements))
+
+
+
     for door_i in range(n_doors):
-        door_offset = doors_offset + door_i * 0xa1
+
+        door_offset = doors_offset + door_i * 0x1a
         first_door_tile_cell_i = int.from_bytes(wed_data[door_offset + 0xa:door_offset + 0xa + 2], "little")
         n_door_tile_cell = int.from_bytes(wed_data[door_offset + 0xc:door_offset + 0xc + 2], "little")
-        print(n_door_tile_cell, first_door_tile_cell_i)
         door_tile_indexes = []
         for door_tile_i in range(n_door_tile_cell):
             tile_offset = door_tile_cell_offset + 2 * (first_door_tile_cell_i + door_tile_i)
             door_tile_indexes.append(int.from_bytes(wed_data[tile_offset:tile_offset + 2], "little"))
 
         print(door_tile_indexes)
+
+
 
 
 
