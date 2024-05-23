c2759c2a79f52841038b8d24b0f6dc0246d0a2d3
Andrey Balabokhin <balabokhin@correlatedsolutions.com>
2024-05-23 17:54:16 -0400

    update cre_morale app

--- cre_morale/src/main.rs
+++ cre_morale/src/main.rs
@@ -1,22 +1,34 @@
 use std::fs;
 use std::env;
 use std::path::Path;
+use std::fs::read_to_string;
+
+fn read_lines(filename: &str) -> Vec<String> {
+    read_to_string(filename)
+        .unwrap()  // panic on possible file-reading errors
+        .lines()  // split the string into an iterator of string slices
+        .map(String::from)  // make each slice into a string
+        .collect()  // gather them together into a vector
+}
 
 fn main() {
     let args : Vec<String> = env::args().collect();
     let filename = &args[1];
-    let data: Vec<u8> = fs::read(filename).unwrap();
+    let mut data: Vec<u8> = fs::read(filename).unwrap();
     let morale_break = u8::from_le_bytes(data[0x240..0x240+1].try_into().unwrap());
     let morale_recovery_time = u16::from_le_bytes(data[0x242..0x242+2].try_into().unwrap());
     print!("{}, {}, {}\n", filename, morale_break, morale_recovery_time);
-/*    let sleep_i_offset = usize::try_from(sleep_i_offset).unwrap();
-    let day_probability:Vec<u8> = data[sleep_i_offset+0xa8..sleep_i_offset+0xa8+2].try_into().unwrap();
-    let night_probability:Vec<u8> = data[sleep_i_offset+0xaa..sleep_i_offset+0xaa+2].try_into().unwrap();
-    let day_probability = u16::from_le_bytes(day_probability.try_into().unwrap());
-    let night_probability = u16::from_le_bytes(night_probability.try_into().unwrap());
-    if night_probability > 0 && day_probability > 0 {
-        let path = Path::new(filename);
-        let filename = path.file_name().unwrap();
-        print!("{}, {}, {}\n", filename.to_str().unwrap(), day_probability, night_probability);
-    }*/
+    let cres = read_lines("cre_to_fix.txt");
+    let new_mb = 5u8.to_le_bytes();
+    let new_mrt = 60u16.to_le_bytes();
+
+    for cre in cres {
+        if filename.contains(&cre) {
+            data[0x240] = new_mb[0];
+            data[0x242] = new_mrt[0];
+            data[0x243] = new_mrt[1];
+            let _ = fs::write(filename, data);
+            break;
+        }
+    }
 }
